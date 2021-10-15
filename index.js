29699227cabc0999a4d7606b0845844cd2d70912
moody-moss <64458544+moody-moss@users.noreply.github.com>
2021-10-15 11:24:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(compression()) // compress HTTP request (compress all routes) [reduce th
 
 app.use(express.text({ limit: '50mb' }))
 
-app.use(cors({ origin: 'https://smartm-dev.es.thingworx.com' }))
+app.use(cors({ origin: ['https://smartm-dev.es.thingworx.com','https://view.vuforia.com'] }))
 app.options('*', cors()) // enable preflight across-the-board --> include bfeore other route
 
 
